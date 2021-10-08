9b00f7fa1f33b97148f93e758245922a737b0687
OsamaTahaluf <91789466+OsamaTahaluf@users.noreply.github.com>
2021-10-08 20:28:09 +0100

    Video 5

--- Navigation/App.js
+++ Navigation/App.js
@@ -11,21 +11,21 @@ import SupportScreen from './screens/SupportScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import BookmarkScreen from './screens/BookmarkScreen';
 
+import RootStackScreen from './screens/RootStackScreen';
 const Drawer = createDrawerNavigator();
 
 const App = () =>{
   return (
     <NavigationContainer>
-
+    <RootStackScreen />
+    
     {/*---------------------------------------------------------------------------- Lazy Loading */}
-      <Drawer.Navigator drawerContent = {props => <DrawerContent {...props}/>}>
-        {/* <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Details" component={Details} /> */}
+      {/* <Drawer.Navigator drawerContent = {props => <DrawerContent {...props}/>}>
         <Drawer.Screen name="MainScreen" component={MainScreen} />
         <Drawer.Screen name="SupportScreen" component={SupportScreen} />
         <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
         <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
-      </Drawer.Navigator>
+      </Drawer.Navigator> */}
 
     {/*---------------------------------------------------------------------------- Normal Loading */}
       {/* <Stack.Navigator
